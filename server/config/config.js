2533017f59828d873cf5c0282565145cc7f2fc9d
Lucas <lucas.gdaniele@gmail.com>
2020-11-20 19:47:52 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,21 @@
 //Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+
+//-------------------------------------
+// Entorno
+//-------------------------------------
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//-------------------------------------
+// Base de datos
+//-------------------------------------
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://LucasDani:3kKgTO8qrlwb21fO@cluster0.8i1su.mongodb.net/test'
+}
+
+process.env.URLDB = urlDB;
