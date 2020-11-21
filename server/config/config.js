a700429d8bea9c3ed3708c356ce9b2710d467d81
Lucas <lucas.gdaniele@gmail.com>
2020-11-21 06:42:30 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://LucasDani:3kKgTO8qrlwb21fO@cluster0.8i1su.mongodb.net/test'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
